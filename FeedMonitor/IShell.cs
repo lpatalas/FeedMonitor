4a5c147056b0adbdb8d214f91c46c399828715a5
Łukasz Patalas <luke.patalas@gmail.com>
2012-07-28 16:39:16 +0200

    Add initial code.

---
+++ FeedMonitor/IShell.cs
@@ -0,0 +1,3 @@
+﻿namespace FeedMonitor {
+    public interface IShell {}
+}
---
+++ FeedMonitor/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace FeedMonitor {
+    using System.ComponentModel.Composition;
+using FeedMonitor.ViewModels;
+
+	[Export(typeof(IShell))]
+	public class ShellViewModel : IShell
+	{
+		public ReaderViewModel CurrentView { get; private set; }
+
+		public ShellViewModel()
+		{
+			this.CurrentView = new ReaderViewModel();
+		}
+	}
+}
