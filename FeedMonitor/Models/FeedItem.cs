bdaa285abbb00a550146f036b0dce338d7d6e9fa
Łukasz Patalas <luke.patalas@gmail.com>
2012-07-28 18:20:57 +0200

    Experiments with UI.

---
+++ FeedMonitor/Models/FeedItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FeedMonitor.Models
+{
+	public class FeedItem
+	{
+		public string Summary { get; set; }
+
+		public string Title { get; set; }
+
+		public static FeedItem FromSyndicationItem(SyndicationItem sourceItem)
+		{
+			return new FeedItem
+			{
+				Summary = ExtractSummary(sourceItem.Summary.Text),
+				Title = sourceItem.Title.Text
+			};
+		}
+
+		public FeedItem()
+		{
+		}
+
+		private static string AddEllipsis(string input)
+		{
+			string result;
+
+			var lastSpaceIndex = input.LastIndexOfAny(new char[] { ' ', '\t' });
+			if (lastSpaceIndex > 0)
+				result = input.Substring(0, lastSpaceIndex);
+			else
+				result = input;
+
+			return result.Trim() + "...";
+		}
+
+		private static string ExtractSummary(string input)
+		{
+			if (string.IsNullOrEmpty(input))
+				return string.Empty;
+
+			var summary = StripTags(input);
+			if (summary.Length > 200)
+				summary = summary.Substring(0, 200);
+
+			return AddEllipsis(summary);
+		}
+
+		private static string StripTags(string input)
+		{
+			if (string.IsNullOrEmpty(input))
+				return string.Empty;
+
+			return Regex.Replace(input, @"<[^>]*>", string.Empty);
+		}
+	}
+}
--- FeedMonitor/ViewModels/ReaderViewModel.cs
+++ FeedMonitor/ViewModels/ReaderViewModel.cs
@@ -1,13 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using Caliburn.Micro;
+using FeedMonitor.Models;
 
 namespace FeedMonitor.ViewModels
 {
-	public class ReaderViewModel
+	public class ReaderViewModel : PropertyChangedBase
 	{
-		public IList<string> Messages { get; set; }
+		public BindableCollection<FeedItem> Results { get; set; }
+
+		public string SourceUrl { get; set; }
+
+		public ReaderViewModel()
+		{
+			this.Results = new BindableCollection<FeedItem>();
+			this.SourceUrl = @"http://blogs.msdn.com/b/ericlippert/rss.aspx";
+		}
+
+		public void Download()
+		{
+			if (string.IsNullOrEmpty(SourceUrl))
+				return;
+
+			using (var reader = XmlTextReader.Create(SourceUrl))
+			{
+				var feed = SyndicationFeed.Load(reader);
+				CopyResults(feed);
+			}
+		}
+
+		private void CopyResults(SyndicationFeed feed)
+		{
+			Results.IsNotifying = false;
+			Results.Clear();
+
+			foreach (var item in feed.Items)
+			{
+				Results.Add(FeedItem.FromSyndicationItem(item));
+			}
+
+			Results.IsNotifying = true;
+			Results.Refresh();
+		}
 	}
 }
