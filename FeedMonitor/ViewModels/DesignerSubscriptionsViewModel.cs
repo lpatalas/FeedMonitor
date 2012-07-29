0aa1279eaa8d09900bb549702e6f57b690cf2c07
Łukasz Patalas <luke.patalas@gmail.com>
2012-07-29 11:49:22 +0200

    Add new files.

---
+++ FeedMonitor/ViewModels/DesignerSubscriptionsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FeedMonitor.Models;
+
+namespace FeedMonitor.ViewModels
+{
+	public class DesignerSubscriptionsViewModel : ISubscriptionsViewModel
+	{
+		private readonly IList<Subscription> subscriptions = new List<Subscription>()
+		{
+			new Subscription("Fabulous Adventures In Coding", @"http://blogs.msdn.com/b/ericlippert/rss.aspx"),
+			new Subscription("The Old New Thing", @"http://blogs.msdn.com/b/oldnewthing/rss.aspx"),
+			new Subscription("Scott Hanselman", @"http://feeds.feedburner.com/ScottHanselman")
+		};
+
+		public IEnumerable<Subscription> Subscriptions
+		{
+			get { return subscriptions; }
+		}
+
+		public void AddSubscription(string url)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemoveSubscription(Models.Subscription subscription)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ FeedMonitor/ViewModels/ISubscriptionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FeedMonitor.Models;
+
+namespace FeedMonitor.ViewModels
+{
+	public interface ISubscriptionsViewModel
+	{
+		IEnumerable<Subscription> Subscriptions { get; }
+
+		void AddSubscription(string url);
+		void RemoveSubscription(Subscription subscription);
+	}
+}
