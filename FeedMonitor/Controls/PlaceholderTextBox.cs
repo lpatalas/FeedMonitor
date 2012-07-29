6f5878561ae2e0c6809b198dba143f9c7c7c4c75
Łukasz Patalas <luke.patalas@gmail.com>
2012-07-29 17:02:47 +0200

    Add placeholder text in new subscription text box.

---
+++ FeedMonitor/Controls/PlaceholderTextBox.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FeedMonitor.Controls
+{
+	public class PlaceholderTextBox : TextBox
+	{
+		public Visibility PlaceholderVisibility
+		{
+			get { return (Visibility)GetValue(PlaceholderVisibilityProperty); }
+			private set { SetValue(PlaceholderVisibilityProperty, value); }
+		}
+
+		public static readonly DependencyProperty PlaceholderVisibilityProperty =
+			DependencyProperty.Register("PlaceholderVisibility", typeof(Visibility), typeof(PlaceholderTextBox), new PropertyMetadata(Visibility.Visible));
+
+		public string PlaceholderText
+		{
+			get { return (string)GetValue(PlaceholderTextProperty); }
+			set { SetValue(PlaceholderTextProperty, value); }
+		}
+
+		public static readonly DependencyProperty PlaceholderTextProperty =
+			DependencyProperty.Register("PlaceholderText", typeof(string), typeof(PlaceholderTextBox), new PropertyMetadata(string.Empty));
+
+		protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+		{
+			base.OnPropertyChanged(e);
+			
+			if (e.Property.Name.Equals("Text") || e.Property.Name.Equals("IsFocused"))
+			{
+				if (Text.Length > 0 || IsFocused)
+					PlaceholderVisibility = Visibility.Hidden;
+				else
+					PlaceholderVisibility = Visibility.Visible;
+			}
+		}
+	}
+}
