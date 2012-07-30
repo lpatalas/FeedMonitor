6a2f7463faed10ff2862023b885cf3e0c4e7980f
Łukasz Patalas <luke.patalas@gmail.com>
2012-07-30 20:54:21 +0200

    Add initial version of message box.

---
+++ FeedMonitor/ViewModels/Designer/DesignerMessageBoxViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedMonitor.ViewModels.Designer
+{
+	class DesignerMessageBoxViewModel : MessageBoxViewModel
+	{
+		public DesignerMessageBoxViewModel()
+			: base(designerMessage, designerTitle)
+		{
+		}
+
+		private const string designerMessage = "Do you really want to remove specified item?";
+		private const string designerTitle = "Confirm removal";
+	}
+}
---
+++ FeedMonitor/ViewModels/MessageBoxViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedMonitor.ViewModels
+{
+	public class MessageBoxViewModel
+	{
+		private readonly string message;
+		private readonly string title;
+
+		public string Message
+		{
+			get { return message; }
+		}
+
+		public string Title
+		{
+			get { return title; }
+		}
+
+		public MessageBoxViewModel(string message, string title)
+		{
+			this.message = message;
+			this.title = title;
+		}
+	}
+}
