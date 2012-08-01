0b19c2b80ddafec6a73af5beb99b77db1fc2b3ca
Łukasz Patalas <luke.patalas@gmail.com>
2012-08-01 21:19:21 +0200

    Add Subscriptions class

---
+++ FeedMonitor.UnitTests/Models/SubscriptionsClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FeedMonitor.Models;
+using FeedMonitor.UnitTests.Fakes;
+using FluentAssertions;
+using Xunit;
+
+namespace FeedMonitor.UnitTests.Models
+{
+	public class SubscriptionsClass
+	{
+		public abstract class Test
+		{
+			protected readonly FakeFeedDownloader feedDownloader;
+			protected readonly FakeFeedFactory feedFactory;
+			protected readonly Subscriptions subscriptions;
+
+			public Test()
+			{
+				feedDownloader = new FakeFeedDownloader();
+				feedFactory = new FakeFeedFactory(feedDownloader);
+				subscriptions = new Subscriptions(feedFactory);
+			}
+		}
+
+		public class AddMethod : Test
+		{
+			[Fact]
+			public void Should_create_feed_for_specified_URL_and_add_it_to_the_list_of_feeds()
+			{
+				// Arrange
+				var url = @"http://blogs.msdn.com/b/ericlippert/rss.aspx";
+
+				// Act
+				subscriptions.Add(url);
+
+				// Assert
+				subscriptions.Feeds.Should().Contain(feed => feed.Url.Equals(url, StringComparison.Ordinal));
+			}
+		}
+
+		public class RemoveMethod : Test
+		{
+			[Fact]
+			public void Should_remove_feed_from_list_of_feeds()
+			{
+				// Arrange
+				var url = @"http://blogs.msdn.com/b/ericlippert/rss.aspx";
+				subscriptions.Add(url);
+
+				var feed = subscriptions.Feeds.First();
+
+				// Act
+				subscriptions.Remove(feed);
+
+				// Assert
+				subscriptions.Feeds.Should().NotContain(feed);
+			}
+		}
+	}
+}
---
+++ FeedMonitor/Models/Subscriptions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FeedMonitor.Services;
+
+namespace FeedMonitor.Models
+{
+	public class Subscriptions
+	{
+		private readonly IFeedFactory feedFactory;
+		private readonly IList<Feed> feeds = new List<Feed>();
+
+		public IList<Feed> Feeds
+		{
+			get { return feeds; }
+		}
+
+		public Subscriptions(IFeedFactory feedFactory)
+		{
+			Contract.Requires(feedFactory != null);
+			this.feedFactory = feedFactory;
+		}
+
+		public void Add(string url)
+		{
+			feeds.Add(feedFactory.Create(url));
+		}
+
+		public void Remove(Feed feed)
+		{
+			feeds.Remove(feed);
+		}
+	}
+}
